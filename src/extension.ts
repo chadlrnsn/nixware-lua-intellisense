c92e5883f6782cc259933c66cac0c34ae290c9f6
chadlrnsn <chadlrnsn@gmail.com>
2024-12-07 00:38:56 +0500

    Implement NixLua language support with configuration and logging enhancements

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,41 @@
 import * as vscode from 'vscode';
 import { NixwareDocumentationParser } from './parser/documentation-parser';
 
+let outputChannel: vscode.OutputChannel;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Создаем канал для логирования
+	outputChannel = vscode.window.createOutputChannel('Nixware Lua');
+	context.subscriptions.push(outputChannel);
+
+	outputChannel.appendLine('Nixware CS2 Lua Extension активировано');
+
 	const parser = new NixwareDocumentationParser();
 	let apiDocs: any = null;
 
 	// Загружаем документацию при активации
 	parser.parseDocumentation().then(docs => {
 		apiDocs = docs;
+		outputChannel.appendLine(`Документация загружена: ${Object.keys(docs.classes).length} классов`);
+		// Показываем канал при успешной загрузке
+		outputChannel.show(true);
+	}).catch(error => {
+		outputChannel.appendLine(`Ошибка загрузки документации: ${error.message}`);
+		// Показываем канал при ошибке
+		outputChannel.show(true);
 	});
 
-	const provider = vscode.languages.registerCompletionItemProvider('lua', {
+	const provider = vscode.languages.registerCompletionItemProvider('nixLua', {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 			if (!apiDocs) {
+				outputChannel.appendLine('API документация еще не загружена');
 				return [];
 			}
+
+			const linePrefix = document.lineAt(position).text.substr(0, position.character);
+			outputChannel.appendLine(`Запрос автодополнения для: ${linePrefix}`);
 
 			const completionItems: vscode.CompletionItem[] = [];
 
@@ -43,6 +62,7 @@ export function activate(context: vscode.ExtensionContext) {
 				});
 			});
 
+			outputChannel.appendLine(`Предложено ${completionItems.length} вариантов автодополнения`);
 			return completionItems;
 		}
 	});
@@ -51,4 +71,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	if (outputChannel) {
+		outputChannel.dispose();
+	}
+}
